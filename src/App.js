04adfb02a1bb94ad19fddcf7c86af8c0e3f6780e
ggang89 <ggang89u@gmail.com>
2024-02-10 12:45:37 +0900

    inputBox에 flex주고 자식요소에 flex:1줘서 크기 지정해줌+h1에 있던 기본 마진을 발견해서 마진을 없애줌

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
     <div className="wrap">
       <h1 className="headerName"> Vite Todo App</h1>
       
+      <div className="inputBox">
+        <input className="inputText" type="text" placeholder="할 일을 입력하세요">
+        </input>
+        <button>추가</button>
+      </div>
     </div>
   );
 }
