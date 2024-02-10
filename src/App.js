af84116ffffdb49b60d5fd72fccd81b6b6919d0f
ggang89 <ggang89u@gmail.com>
2024-02-10 12:59:00 +0900

    리스트를 만들어서 내용을 넣어주고, ui속성 만듦->display flex하고 align-items를 중앙정렬해서 늘어진 크기를 조정해줌

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ function App() {
   return (
     <div className="wrap">
       <h1 className="headerName"> Vite Todo App</h1>
-      
+
       <div className="inputBox">
-        <input className="inputText" type="text" placeholder="할 일을 입력하세요">
-        </input>
+        <input
+          className="inputText"
+          type="text"
+          placeholder="할 일을 입력하세요"
+        ></input>
         <button>추가</button>
       </div>
+
+      <ul >
+        <li className="box">
+          <p className="textBox">리액트 배우기 열심히</p>
+          <button >수정</button>
+          <button>삭제</button>
+        </li>
+      </ul>
     </div>
   );
 }
