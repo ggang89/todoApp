904811d6ffffe953e2164f0df6005c065ff11785
ggang89 <ggang89u@gmail.com>
2024-02-10 14:37:08 +0900

    리스트 내용변경을 조건문으로 만들어줌, 텍스트창과 버튼창은 모두 하나의 객체안의 키와 값이다

--- src/App.js
+++ src/App.js
@@ -9,7 +9,13 @@ import React, { useState } from "react";
 //3. 텍스트 입력창이 생긴다.
 //4 . 수정버튼을 다시 누르면 내용이 저장되고 삭제버튼이 생긴다
 function App() {
-  const [todo, setTodo] = useState({todoTitle:"리액트 배우기"});
+  const [todo, setTodo] = useState({
+    todoTitle: "리액트 배우기",
+    isEditing: false,
+  });
+  //1.isEditing=true이면 input창을 보여준다.
+  //2. false면 그대로 있다
+
   //1. 기본값을 문자로 넣어준 것을 객체로 바꿔줌.
   //2.연결된 변수를 객체의 키값으로 변경해줌
 
@@ -28,22 +34,28 @@ function App() {
 
       <ul>
         <li className="box">
-          <p className="textBox">{todo.todoTitle}</p>
-          {/* {변수명}에 변경되는 값이 들어간다.글자가 보이는 자리에 연결해준다 */}
-          <button className="button">수정</button>
-          <button>삭제</button>
+          {todo.isEditing ? (
+            <>
+              <input className="updateInput" value={todo.todoTitle} />
+              {/* input태그의 값을 받기 위해서 value로 변수값을 받아온다! */}
+              {/* 객체의 키값으로 변수를 받아옴 */}
+              <button>수정</button>
+            </>
+          ) : (
+            <>
+              <p className="textBox">{todo.todoTitle}</p>
+              {/* {변수명}에 변경되는 값이 들어간다.글자가 보이는 자리에 연결해준다 */}
+              <button className="button">수정</button>
+              <button>삭제</button>
+            </>
+          )}
         </li>
         {/* <li className="box">
           <p className="textBox">타입스크립트 배우기</p>
           <button>수정</button>
           <button>삭제</button>
         </li> */}
-        <li className="box">
-          <input className="updateInput" value={todo.todoTitle} />
-          {/* input태그의 값을 받기 위해서 value로 변수값을 받아온다! */}
-          {/* 객체의 키값으로 변수를 받아옴 */}
-          <button>수정</button>
-        </li>
+        <li className="box"></li>
       </ul>
     </div>
   );
